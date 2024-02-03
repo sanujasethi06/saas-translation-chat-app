f3c90bf1c8489de4ec95970ed0cd1c6a8111f765
sanujasethi06 <sethisanuja1998@gmail.com>
2024-02-03 11:12:46 +0530

    Adding checkout loading

--- components/CheckoutIcon.tsx
+++ components/CheckoutIcon.tsx
@@ -1,17 +1,24 @@
 "use client"
 import { useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useState } from 'react'
 
 const CheckoutIcon = () => {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
+  
   const createCheckOutSession = async () => {
-    if (!session) return;
+    if (!session?.user.id) return;
+    setLoading(true);
+    
    }
 
   return (
-    <button className='mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80'>
+    <div className="flex flex-col space-y-2">
+
+      <button onClick={() => createCheckOutSession()} className='mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80'>
       Sign Up
     </button>
+    </div>
   )
 }
 
